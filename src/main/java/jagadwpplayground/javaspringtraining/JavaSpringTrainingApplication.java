a1b0fd5e01522f33bc0ba53c5197f64ce4984dfe
jagadwp <jagadpurnomo26@gmail.com>
2023-11-29 17:32:52 +0700

    feat: add entities

---
+++ src/main/java/jagadwpplayground/javaspringtraining/JavaSpringTrainingApplication.java
@@ -0,0 +1,14 @@
+package jagadwpplayground.javaspringtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTrainingApplication.class, args);
+		System.out.println("Bismillah");
+	}
+
+}
---
+++ src/test/java/jagadwpplayground/javaspringtraining/JavaSpringTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package jagadwpplayground.javaspringtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
